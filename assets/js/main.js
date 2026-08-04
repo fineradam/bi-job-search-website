c704021576ffdd2079a4c7595e3ef89664c781a6
fineradam <adam@vitaminbi.com>
2026-08-04 15:15:04 +0200

    Fix GA4 page view tracking

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,10 +77,29 @@ function loadGoogleAnalytics() {
   script.async = true;
   script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
   script.dataset.ga4Loader = "true";
-  document.head.appendChild(script);
 
-  window.gtag("js", new Date());
-  window.gtag("config", GA_MEASUREMENT_ID);
+  script.addEventListener("load", () => {
+    window.gtag("consent", "update", {
+      analytics_storage: "granted",
+      ad_storage: "denied",
+      ad_user_data: "denied",
+      ad_personalization: "denied"
+    });
+
+    window.gtag("js", new Date());
+
+    window.gtag("config", GA_MEASUREMENT_ID, {
+      send_page_view: false
+    });
+
+    window.gtag("event", "page_view", {
+      page_title: document.title,
+      page_location: window.location.href,
+      page_path: window.location.pathname
+    });
+  });
+
+  document.head.appendChild(script);
 }
 
 function deleteCookie(name, domain) {
